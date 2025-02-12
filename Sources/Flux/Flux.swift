3628347d0b4ab317e1fa1e5d0f90cdfdc1abc1b9
Manuel Selch <manuel@selch.de>
2025-02-12 16:57:00 +0100

    init

--- Sources/Flux/Flux.swift
+++ Sources/Flux/Flux.swift
@@ -1,2 +1,41 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public struct Flux {
+    public protocol Feature {
+        associatedtype State
+        associatedtype Action
+    }
+    
+    public typealias StoreOf<F: Feature> = BaseStore<F.State, F.Action>
+    
+    
+    public typealias Middleware<State, Action> = (State, Action, @escaping (Action) -> ()) -> ()
+}
+
+public class BaseStore<S, A>: ObservableObject {
+    @Published public private(set) var state: S
+    private let stateQueue = DispatchQueue(label: "flux.state.queue", qos: .userInitiated)
+    private let middlewares: [Flux.Middleware<S, A>]
+    
+    init(state: S, middlewares: [Flux.Middleware<S, A>]) {
+        self.state = state
+        self.middlewares = middlewares
+    }
+    
+    public func dispatch(_ action: A) {
+        for middleware in self.middlewares {
+            middleware(self.state, action) { newAction in
+                self.dispatch(newAction)
+            }
+        }
+
+        stateQueue.sync {
+            self.reduce(&self.state, action)
+        }
+    }
+    
+    func reduce(_ state: inout S, _ action: A) {
+        
+    }
+}
+
