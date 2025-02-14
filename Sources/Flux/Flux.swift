a938edc628ea9cb01e9f455d1c876ccde745ac9e
Manuel Selch <manuel@selch.de>
2025-02-14 19:29:54 +0100

    middleware async (without combine publisher)

--- Sources/Flux/Flux.swift
+++ Sources/Flux/Flux.swift
@@ -3,41 +3,42 @@ import Combine
 
 public protocol Feature {
     associatedtype State
-    associatedtype Action
+    associatedtype Action: Sendable
 }
 
 public typealias StoreOf<F: Feature> = BaseStore<F.State, F.Action>
-public typealias Middleware2<State, Action> = (State, Action) -> AnyPublisher<Action, Never>
-public typealias Middleware<State, Action> = @Sendable (State, Action, @escaping @Sendable (Action) -> ()) -> ()
+public typealias Middleware<State, Action> =
+    @Sendable (State, Action) async -> (Action?)
 
 
-open class BaseStore<S, A: Sendable>: ObservableObject, @unchecked Sendable {
+open class BaseStore<S, A: Sendable>: ObservableObject {
     public typealias State = S
     public typealias Action = A
     public typealias M = Middleware<S, A>
     
     @Published public private(set) var state: S
     private let stateQueue = DispatchQueue(label: "flux.state.queue", qos: .userInitiated)
-    private let middlewares: [Flux.Middleware<S, A>]
-    private let middlewares2: [Flux.Middleware2<S, A>] = []
+    private let middlewares: [M]
     
     var cancellables: Set<AnyCancellable> = []
     
-    public init(state: S, middlewares: [Flux.Middleware<S, A>]) {
+    public init(state: S, middlewares: [M]) {
         self.state = state
         self.middlewares = middlewares
     }
     
+    @MainActor
     public func dispatch(_ action: A) {
-        for middleware in self.middlewares {
-            middleware(self.state, action) { @Sendable action in
+        self.reduce(&self.state, action)
+    
+        middlewares.forEach { middleware in
+            Task {
+                guard let action = await middleware(state, action) else { return }
                 Task { @MainActor [self] in
                     self.dispatch(action)
                 }
             }
         }
-
-        self.reduce(&self.state, action)
     }
     
     open func reduce(_ state: inout S, _ action: A) {
@@ -45,9 +46,8 @@ open class BaseStore<S, A: Sendable>: ObservableObject, @unchecked Sendable {
     }
 }
 
-
 /*
-public extension AnyPublisher where Output: Sendable {
+public extension AnyPublisher {
     static func dispatch(_ action: Output) -> AnyPublisher<Output, Failure> {
         return Just(action)
             .setFailureType(to: Failure.self)
@@ -66,39 +66,14 @@ public extension AnyPublisher where Output: Sendable {
     }
     
     static func run(
-        _ operation: @escaping (
-            @Sendable (Output) -> Void
-        ) async -> Void
+        _ operation: @escaping @Sendable () -> (Output?)
     ) -> AnyPublisher<Output, Failure>
     {
-        return Future<Output, Failure> { promise in
-     
-            nonisolated(unsafe) let promise = promise
-            
-            Task { @MainActor [promise] in
-                await operation {
-                    promise(.success($0))
-                }
-            }
-        }
-        .eraseToAnyPublisher()
-    }
-    
-    static func call(
-        _ operation: @escaping (
-            @Sendable (Output) -> Void
-        ) async -> Void
-    ) -> AnyPublisher<Output, Failure> {
+        
         return Future { promise in
-
-            // Copy promise to a local property to make it nonisolated(unsafe):
-            nonisolated(unsafe) let promise = promise
-
-            Task { @MainActor [promise] in
-                // Now capture promise in a Sendable closure.
-                
-                operation {
-                    promise(.success($0))
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                if let result = operation() {
+                    promise(.success(result))
                 }
             }
         }.eraseToAnyPublisher()
